ecfb63b084af3d5b315058d97712e2691a738482
rxluz <rx@appock.co>
2016-01-06 00:59:55 -0200

    remove some files

--- routes/index.js
+++ routes/index.js
@@ -19,42 +19,6 @@ router.get('/', function(req, res) {
   });
 });
 
-/* GET speakers page. */
-router.get('/speakers', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-  myArtists = appdata.speakers;
-
-  appdata.speakers.forEach(function(item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
-  res.render('speakers', {
-    title: 'Speakers',
-    artwork: myArtwork,
-    artists: myArtists,
-    page: 'artistslist'
-  });
-});
-
-
-/* GET speakers page. */
-router.get('/speakers/:speakerid', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-
-  appdata.speakers.forEach(function(item) {
-    if (item.shortname == req.params.speakerid) {
-      myArtists.push(item);
-      myArtwork = myArtwork.concat(item.artwork);
-    }
-  });
-  res.render('speakers', {
-    title: 'Speakers',
-    artwork: myArtwork,
-    artists: myArtists,
-    page: 'artistsDetail'
-  });
-});
 
 
 
