447262452a7731c33089c606dc49087bd487ba41
rxluz <rx@appock.co>
2016-01-07 01:13:42 -0200

    integration with browserify

---
+++ components/scripts/app.js
@@ -0,0 +1,52 @@
+
+ var angular = require("angular");
+ require("angular-animate/angular-animate");
+ require('angular-route/angular-route');
+//alert('helload');
+
+//var message="hello";
+
+//#1
+var myApp = angular.module('myApp',
+  ['ngRoute', 'firebase'])
+  .constant('FIREBASE_URL', 'https://appockangdata77.firebaseio.com/');
+
+
+myApp.run(['$rootScope', '$location',
+  function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError',
+      function(event, next, previous, error) {
+        if (error=='AUTH_REQUIRED') {
+          $rootScope.message = 'Sorry, you must log in to access that page';
+          $location.path('/login');
+        } // AUTH REQUIRED
+      }); //event info
+  }]); //run
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'RegistrationController'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'RegistrationController'
+    }).
+    when('/checkins/:uId/:mId', {
+      templateUrl: 'views/checkins.html',
+      controller: 'CheckInsController'
+    }).
+    when('/meetings', {
+      templateUrl: 'views/meetings.html',
+      controller: 'MeetingsController',
+      resolve: {
+        currentAuth: function(Authentication) {
+          return Authentication.requireAuth();
+        } //current Auth
+      } //resolve
+    }).
+    otherwise({
+      redirectTo: '/login'
+    });
+}]);
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    minifyCss = require('gulp-minify-css'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
@@ -10,28 +11,54 @@ var gulp = require('gulp'),
     pngcrush = require('imagemin-pngcrush'),
     concat = require('gulp-concat'),
     htmlmin = require('gulp-html-minifier'),
-    bower = require('gulp-bower'),
+    //bower = require('gulp-bower'),
     del = require('del'),
-    git = require("gulp-git"),
+    //git = require("gulp-git"),
     pipe = require("gulp-pipe");
 
 var env,
     bowerSources,
     htmlSources,
+    cssSources,
     jsSources,
     sassSources,
     jsonSources,
     outputDir,
+    outputDirDev,
     sassStyle;
 
-
 ///sassStyle = 'compressed';
+outputDirDev = './builds/development/';
+outputDir = './builds/production/';
 
+viewsSources = ['./components/views/*.html'];
 htmlSources = ['./components/html/*.html'];
 bowerSources = ['./bower_components/**/*.*'];
-jsSources = ['./components/scripts/*.js'];
+jsSources = ['./components/scripts/app.js', './components/scripts/services/authentication.js', './components/scripts/controllers/*.js'];
 sassSources = ['./components/sass/style.scss'];
 jsonSources = ['./components/scripts/*.json'];
+cssSources = ['./components/css/*.css'];
+
+
+
+/*
+ * Generate views files in dev mode
+ */
+gulp.task('views', function() {
+  del(outputDirDev + 'views/*.html');
+  gulp.src(viewsSources)
+    .pipe(gulp.dest(outputDirDev+"views/"))
+});
+
+/*
+ * Generate html files in prod mode
+ */
+gulp.task('views_prod', function() {
+  del(outputDir + 'views/*.html');
+  gulp.src(viewsSources)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(outputDir+"views/"))
+});
 
 
 
@@ -39,9 +66,9 @@ jsonSources = ['./components/scripts/*.json'];
  * Generate html files in dev mode
  */
 gulp.task('html', function() {
-  del(outputDir + '*.html');
+  del(outputDirDev + '*.html');
   gulp.src(htmlSources)
-    .pipe(gulp.dest(outputDir))
+    .pipe(gulp.dest(outputDirDev))
 });
 
 /*
@@ -59,11 +86,15 @@ gulp.task('html_prod', function() {
  * Generate js files in dev mode
  */
 gulp.task('js', function() {
-  del(outputDir + 'js/**/*.*');
+  del(outputDirDev + 'js/**/*.*');
 
   gulp.src(jsSources)
     .pipe(concat('script.js'))
-    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(browserify({
+      insertGlobals : true,
+      debug : !gulp.env.production
+    }))
+    .pipe(gulp.dest(outputDirDev + 'js'))
     .pipe(connect.reload())
 });
 
@@ -75,37 +106,74 @@ gulp.task('js_prod', function() {
 
   gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify({
+      insertGlobals : true,
+      debug : !gulp.env.production
+    }))
+
     .pipe(uglify())
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
 });
 
 /*
- * Generate bower files
+ * Generate css files in prod mode
  */
-gulp.task('bower', function() {
-  del(outputDir + 'libs/**/**');
+gulp.task('css_prod', function() {
+  del(outputDir + 'css/stylec.css');
 
-  gulp.src(bowerSources)
-    .pipe(gulp.dest(outputDir + 'libs'))
+  gulp.src(cssSources)
+    .pipe(concat('stylec.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(outputDir + 'css'))
     .pipe(connect.reload())
 });
+
+/*
+ * Generate css files in dev mode
+ */
+gulp.task('css', function() {
+  del(outputDirDev + 'css/stylec.css');
+
+  gulp.src(cssSources)
+    .pipe(concat('stylec.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(outputDirDev + 'css'))
+    .pipe(connect.reload())
+});
+
+
+// /*
+//  * Generate bower files
+//  */
+// gulp.task('bower', function() {
+//   del(outputDir + 'libs/**/**');
+//   del(outputDirDev + 'libs/**/**');
+//
+//   gulp.src(bowerSources)
+//     .pipe(gulp.dest(outputDir + 'libs'))
+//     .pipe(connect.reload());
+//
+//     gulp.src(bowerSources)
+//       .pipe(gulp.dest(outputDirDev + 'libs'))
+//       .pipe(connect.reload());
+// });
 
 
 /*
  * Generate sass files in dev mode
  */
 gulp.task('compass', function() {
-  del(outputDir + 'css/**/*.*');
+  del(outputDirDev + 'css/style.css');
 
   gulp.src(sassSources)
     .pipe(compass({
       sass: 'components/sass',
-      image: outputDir + 'images',
+      image: outputDirDev + 'images',
       style: 'expanded'
     })
     .on('error', gutil.log))
-    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(gulp.dest(outputDirDev + 'css'))
     .pipe(connect.reload())
 });
 
@@ -113,7 +181,7 @@ gulp.task('compass', function() {
  * Generate sass files in prod mode
  */
 gulp.task('compass_prod', function() {
-  del(outputDir + 'css/**/*.*');
+  del(outputDir + 'css/style.css');
 
   gulp.src(sassSources)
     .pipe(compass({
@@ -132,7 +200,7 @@ gulp.task('compass_prod', function() {
  */
 gulp.task('connect', function() {
   connect.server({
-    root: outputDir,
+    root: outputDirDev,
     livereload: true
   });
 });
@@ -143,8 +211,7 @@ gulp.task('connect', function() {
 
 gulp.task('json', function() {
   gulp.src('components/scripts/*.json')
-    .pipe(jsonminify())
-    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(gulp.dest(outputDirDev + 'js'))
     .pipe(connect.reload())
 });
 
@@ -153,6 +220,7 @@ gulp.task('json', function() {
  */
 gulp.task('json_prod', function() {
   gulp.src('components/scripts/*.json')
+    .pipe(jsonminify())
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
 });
@@ -163,7 +231,7 @@ gulp.task('json_prod', function() {
  */
 gulp.task('images', function() {
   gulp.src('components/images/*.*')
-    .pipe(gulp.dest(outputDir + 'images'))
+    .pipe(gulp.dest(outputDirDev + 'images'))
     .pipe(connect.reload())
 });
 
@@ -191,14 +259,14 @@ gulp.task('dev', function() {
   //dont compress csss
   //dont compress js
 
-  outputDir = './builds/development/';
+  gulp.start('js',  'json', 'css', 'compass', 'images', 'connect', 'html', 'views');
 
-  gulp.start('js', 'bower', 'json', 'compass', 'images', 'connect', 'html');
-
-  gulp.watch('bower_components/**/*.*', ['bower']);
+  // gulp.watch('bower_components/**/*.*', ['bower']);
   gulp.watch('components/html/*.html', ['html']);
+  gulp.watch('components/views/*.html', ['views']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/js/*.json', ['json']);
+  gulp.watch('components/css/*.css', ['css']);
   gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('components/images/*.*', ['images']);
 });
@@ -213,15 +281,18 @@ gulp.task('prod', function() {
   //compress csss
   //compress js
 
-  outputDir = './builds/production/';
+  gulp.start('js_prod', 'json_prod', 'css_prod', 'compass_prod', 'images_prod', 'html_prod', 'views_prod');
 
-  gulp.start('js_prod', 'bower', 'json_prod', 'compass_prod', 'images_prod', 'connect', 'html_prod');
-
-  outputDir = './builds/production/';
-  gulp.watch('bower_components/**/*.*', ['bower']);
   gulp.watch('components/html/*.html', ['html_prod']);
+  gulp.watch('components/views/*.html', ['views_prod']);
   gulp.watch(jsSources, ['js_prod']);
   gulp.watch('components/js/*.json', ['json_prod']);
+  gulp.watch('components/css/*.css', ['css_prod']);
   gulp.watch('components/sass/*.scss', ['compass_prod']);
   gulp.watch('components/images/*.*', ['images_prod']);
+});
+
+
+gulp.task('default', function() {
+  gulp.start('prod', 'dev');
 });
